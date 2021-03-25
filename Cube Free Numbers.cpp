9af3f4dd7ec7be4bb0450a8108f6f53b1b04ba9a
Worawat12322 <71426736+Worawat12322@users.noreply.github.com>
2021-03-25 17:34:03 +0700

    Update Cube Free Numbers.cpp

--- Cube Free Numbers.cpp
+++ Cube Free Numbers.cpp
@@ -1,10 +1,28 @@
 #include<stdin.h>
 int main(){
-	int ent;
+	int ent,chk,count=0;
+	int i,l;
 	scanf("%d",&ent);
 	if(ent>=1&&ent<=1000000)
 	{
-		
+		for(i=2;i<ent&&count!=3;i++)
+		{
+			count=0;
+			chk = ent;
+			for(l=1;l<4;l++)
+			{
+				if(chk%i==0)
+				{
+					count++;
+					chk/i;
+				}
+			}
+		}
+		if(count==3)
+		{
+			printf("Not Cube Free");
+		}
+		else printf("%d",ent);
 	}
 	return 0;
 }
